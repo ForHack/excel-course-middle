c4ec617edb4084bc9d1ead6ae322e2f7596a0014
odiljon27 <olimjonov-odiljon@mail.ru>
2021-09-04 16:05:45 +0500

    Finish redux and table logic

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,30 +1,47 @@
-import { ExcelComponent } from "@core/ExcelComponent";
-
+import {ExcelComponent} from '@core/ExcelComponent'
+import { defaultTitle } from '../../constants'
+import { $ } from '../../core/dom'
+import { changeTitle } from '../../redux/actions'
+import { debounce } from '../../core/utils'
 export class Header extends ExcelComponent {
-    static className = 'excel__header'
+  static className = 'excel__header'
 
-    constructor($root, options) {
-      super($root, {
-         name: 'Header',
-         ...options 
-      }) 
-    }
+  constructor($root, options) {
+    super($root, {
+      name: 'Header',
+      listeners: ['input'],
+      ...options,
+    })
+  }
 
-     toHTML() {
-        return `
-                <input type="text" class="input" value="Новая таблица" />
+  
 
-                <div>
+  prepare() {
+    this.onInput = debounce(this.onInput, 300)
+  }
 
-                    <div class="button">
-                        <i class="material-icons">delete</i>
-                    </div>
+  toHTML() {
+    const title = this.store.getState().title || defaultTitle
 
-                    <div class="button">
-                        <i class="material-icons">exit_to_app</i>
-                    </div>
+    return `
+      <input type="text" class="input" value="${title}" />
 
-                </div>
-        `
-    }
+      <div>
+
+        <div class="button">
+          <i class="material-icons">delete</i>
+        </div>
+
+        <div class="button">
+          <i class="material-icons">exit_to_app</i>
+        </div>
+
+      </div>
+    `
+  }
+
+  onInput(event) {
+    const $target = $(event.target)
+    this.$dispatch(changeTitle($target.text()))
+  }
 }
